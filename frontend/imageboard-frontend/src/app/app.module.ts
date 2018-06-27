2345d4e22d259c37aeb447d6b1725c52c0d58284
unlogicalcode <jearsmail99@gmail.com>
2018-06-27 09:25:36 +0200

    frontend: finished Header-Styling

--- frontend/imageboard-frontend/src/app/app.module.ts
+++ frontend/imageboard-frontend/src/app/app.module.ts
@@ -2,7 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 // Angular-Material imports
-import { MatButtonModule, MatIconModule, MatMenuModule, MatToolbarModule } from '@angular/material';
+import { MatButtonModule, MatMenuModule, MatToolbarModule, MatTabsModule } from '@angular/material';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
@@ -10,20 +12,22 @@ import { HeaderComponent } from './header/header.component';
 import { ContentComponent } from './content/content.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    ContentComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    MatButtonModule,
-    MatIconModule,
-    MatMenuModule,
-    MatToolbarModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HeaderComponent,
+		ContentComponent
+	],
+	imports: [
+		BrowserModule,
+		BrowserAnimationsModule,
+		MatButtonModule,
+		MatIconModule,
+		MatMenuModule,
+		MatToolbarModule,
+		MatTabsModule,
+		MatInputModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
