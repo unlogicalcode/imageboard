7a39f97e8fe8bc189ea1065d18ca77b11e489492
unlogicalcode <jearsmail99@gmail.com>
2018-07-03 21:37:16 +0200

    frontend: adjusted category properties

--- frontend/imageboard-frontend/src/entity/category.ts
+++ frontend/imageboard-frontend/src/entity/category.ts
@@ -1,10 +1,15 @@
 export class Category {
 	displayName: string;
 	uuid: string;
+	primary: boolean;
 
 	constructor(name: string, uuid: string) {
 		this.displayName = name;
 		this.uuid = uuid;
+	}
+
+	setPrimary() {
+		this.primary = !this.primary;
 	}
 
 }
