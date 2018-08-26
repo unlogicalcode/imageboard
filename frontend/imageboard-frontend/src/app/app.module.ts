4fd2a1808787e06638115b9b3378e76cccaa1c76
unlogicalcode <jearsmail99@gmail.com>
2018-08-26 15:21:01 +0200

    frontend cleanup of app.module

--- frontend/imageboard-frontend/src/app/app.module.ts
+++ frontend/imageboard-frontend/src/app/app.module.ts
@@ -1,21 +1,29 @@
+//angular base immports
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 // Angular-Material imports
-import { MatButtonModule, MatMenuModule, MatToolbarModule, MatTabsModule } from '@angular/material';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import {MatGridListModule} from '@angular/material/grid-list';
 
+//own component imports
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ContentComponent } from './content/content.component';
-
-import { CategorieService } from './../service/categorie.service';
 import { ImageViewComponent } from './image-view/image-view.component';
+
+//own service imports
+import { CategorieService } from './../service/categorie.service';
+
+//module imports
 import { AppRoutingModule } from './/app-routing.module';
 
 @NgModule({
